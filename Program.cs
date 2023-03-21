21aebcdd71f197d1ca1cc84ea732d9eb0433d68a
Shinshi11a <pro100vanyok2016@gmail.com>
2023-03-21 12:09:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+interface IHello
+{
+    void SayHello();
+}
+
+class EnglishHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+}
+
+class FrenchHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Bonjour");
+    }
+}
+
+class SpanishHello : IHello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hola");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IHello> hellos = new List<IHello>();
+        hellos.Add(new EnglishHello());
+        hellos.Add(new FrenchHello());
+        hellos.Add(new SpanishHello());
+
+        foreach (IHello hello in hellos)
+        {
+            hello.SayHello();
+        }
+    }
+}
